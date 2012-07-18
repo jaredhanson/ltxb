658ca7a31f40a2ebbd510a3084ab1bb8fc3e2948
Jared Hanson <jaredhanson@gmail.com>
2012-07-18 08:18:13 -0700

    Additional test cases.

--- test/index-test.js
+++ test/index-test.js
@@ -53,4 +53,80 @@ vows.describe('ltxb').addBatch({
     },
   },
   
+  'render file with self option': {
+    topic: function() {
+      var self = this;
+      var options = { name: 'world', self: true };
+      ltxb(__dirname + '/fixtures/hello-self.ltxb', options, function(err, res) {
+        self.callback(err, res);
+      });
+    },
+    
+    'should not error' : function(err, res) {
+      assert.isNull(err);
+    },
+    'should render correctly' : function(err, res) {
+      assert.equal(res, '<greetings><hello name="world"/></greetings>');
+    },
+  },
+  
+  'render file without locals': {
+    topic: function() {
+      var self = this;
+      ltxb(__dirname + '/fixtures/static.ltxb', function(err, res) {
+        self.callback(err, res);
+      });
+    },
+    
+    'should not error' : function(err, res) {
+      assert.isNull(err);
+    },
+    'should render correctly' : function(err, res) {
+      assert.equal(res, '<beep><boop/></beep>');
+    },
+  },
+  
+  'useful stack traces': {
+    topic: function() {
+      var self = this;
+      
+      var str = [
+        "xml.n('root')",
+        "  .c('hello', {'name': name })" // Failing line 
+      ].join("\n");
+      
+      ltxb.render(str, function(err, res) {
+        self.callback(err, res);
+      });
+    },
+    
+    'should error' : function(err, res) {
+      assert.strictEqual(err.name, 'ReferenceError');
+      assert.include(err.message, 'name is not defined');
+      assert.include(err.message, 'ltxb:');
+    },
+  },
+  
+  'non useful stack traces': {
+    topic: function() {
+      var self = this;
+      var options = { compileDebug: false };
+      
+      var str = [
+        "xml.n('root')",
+        "  .c('hello', {'name': name })" // Failing line 
+      ].join("\n");
+      
+      ltxb.render(str, options, function(err, res) {
+        self.callback(err, res);
+      });
+    },
+    
+    'should error' : function(err, res) {
+      assert.strictEqual(err.name, 'ReferenceError');
+      assert.include(err.message, 'name is not defined');
+      assert.equal(err.message.indexOf('ltxb:'), -1);
+    },
+  },
+  
 }).export(module);
